d2453b32d9ed96146200a0abef5716e6a76ba82f
Charles-Edouard Coste <cc@synap.fr>
2011-12-16 21:14:42 +0100

    Adding accessible placeholder JQuery plugin

---
+++ src/placeholder/jquery.placeholder.js
@@ -0,0 +1,21 @@
+/**
+ * Placeholder plugin for jQuery
+ *
+ * @copyright 2011 Synap System (http://synap.fr)
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+(function( $ ) {
+
+  $.fn.placeholder = function(placeholder) {
+
+    return this.each(function(){
+
+        var hasChanged  = false;
+
+        $(this).val(placeholder)
+               .change(function(){hasChanged = true; })
+               .focusin(function(){if(!hasChanged){$(this).val(''); }})
+               .focusout(function(){if(!hasChanged || !$(this).val()){ $(this).val(placeholder); hasChanged = false; } });        
+    });
+  };
+})( jQuery );
